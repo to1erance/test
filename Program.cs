7e860bf6becc0b41f66f954d2a4984219e49a155
to1erance <166586943+to1erance@users.noreply.github.com>
2024-04-10 10:59:09 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите б");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите действие");
+int act = int.Parse(Console.ReadLine());
+double res = 0;
+
+switch (act)
+{
+    case 1:
+        res = a * b;
+    break;
+    case 2:
+        res = a / b;
+        break;
+    case 3:
+        res = a + b;
+        break;
+    case 4:
+        res = a - b;
+        break;
+}
+Console.WriteLine(act);
